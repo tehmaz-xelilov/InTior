f543e70f7f48af2fae4767921b6e7feaeb55809e
tehmaz-xelilov <tehmazxelilov124@gmail.com>
2026-05-13 18:18:53 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// 1 ci tapsiriq
+
+let qiymet1 = "10";
+let qiymet2 = "20";
+let cem = Number(qiymet1) + Number(qiymet2);
+console.log(cem);
+
+
+
+// 2 ci tapsiriq
+let mehsulAdi = "kiwi";
+let mehsulQiymet = 5;
+let endirimFaizi = 20;
+let stokdaVarmi = true;
+
+let sonQiymet = mehsulQiymet * (100 - endirimFaizi) / 100;
+
+console.log("Mehsul: " + mehsulAdi + "\nEndirimli Qiymət: " + sonQiymet)
